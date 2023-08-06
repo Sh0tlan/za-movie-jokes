624660ed2d50ce69e762cbd22cef85a750abfe41
Sh0tlan <x359623@ukr.net>
2023-08-06 13:53:29 +0300

    add Initial Layout setup with ChackraUI

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,21 @@
+import { Route, Routes } from "react-router";
+import MovieList from "src/features/movies/MovieList";
+import MovieDetails from "src/features/movies/MovieDetails";
+
+import Layout from "src/common/ui/Layout";
+import { ChakraProvider } from "@chakra-ui/react";
+
 function App() {
-  return <h1>Hi</h1>;
+  return (
+    <ChakraProvider>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MovieList />} />
+          <Route path="movies/:movieId" element={<MovieDetails />} />
+        </Route>
+      </Routes>
+    </ChakraProvider>
+  );
 }
 
 export default App;
