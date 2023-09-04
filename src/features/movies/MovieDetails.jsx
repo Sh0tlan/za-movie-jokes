d9a66ff695923d34829382f0606d80bc56a39ab3
Kysliak <x359623@ukr.net>
2023-09-04 23:06:31 +0300

    Movies Fetching with RTK Query

--- src/features/movies/MovieDetails.jsx
+++ src/features/movies/MovieDetails.jsx
@@ -4,26 +4,76 @@ import {
   Card,
   CardBody,
   CardFooter,
+  Flex,
   Heading,
   IconButton,
   Image,
+  Spinner,
   Stack,
   Text,
 } from "@chakra-ui/react";
 import { ArrowBackIcon } from "@chakra-ui/icons";
 import { Link, useParams } from "react-router-dom";
 import { MOVIEDB_IMAGES_URL } from "src/common/constants";
-import { useSelector } from "react-redux";
-import { selectMovieById } from "./moviesSlice";
 import JokesGenerator from "../ai-jokes/JokesGenerator";
+import { useGetMovieByIdQuery } from "./moviesApi";
 
 function MovieDetails() {
   const { movieId } = useParams();
+  const { data, isError, error, isLoading, isSuccess } =
+    useGetMovieByIdQuery(movieId);
 
-  const movie = useSelector((state) => selectMovieById(state, movieId));
-  console.log(movie);
+  let content;
+
+  if (isSuccess) {
+    content = (
+      <Card
+        direction={{ base: "column", sm: "row" }}
+        overflow="hidden"
+        variant="outline"
+      >
+        <Image
+          objectFit="cover"
+          maxW={{ base: "100%", sm: "200px" }}
+          src={`${MOVIEDB_IMAGES_URL}/${data.poster_path}`}
+          alt={`${data.title}'s poster`}
+        />
+
+        <Stack>
+          <CardBody>
+            <Heading size="md">{data.title}</Heading>
+            <Text py="2" color="gray.600">
+              Rating: <Badge colorScheme="yellow">{data?.vote_average}</Badge>
+            </Text>
+            <Text py="2">{data.overview}</Text>
+          </CardBody>
+
+          <CardFooter>
+            <JokesGenerator
+              movieId={data.id}
+              movieTitle={data.title}
+              movieDescription={data.overview}
+            />
+          </CardFooter>
+        </Stack>
+      </Card>
+    );
+  } else if (isLoading) {
+    content = (
+      <Flex alignItems="center" justifyContent="center" minH="100vh">
+        <Spinner text="Loading..." />
+      </Flex>
+    );
+  } else if (isError) {
+    content = (
+      <Flex alignItems="center" justifyContent="center">
+        {error?.data?.status_message ?? "Something went wrong"}
+      </Flex>
+    );
+  }
+
   return (
-    <Box minH="100vh">
+    <Box minH="100vh" minW="100%" py={8}>
       <Link to="/">
         <IconButton
           aria-label="Go back"
@@ -33,37 +83,8 @@ function MovieDetails() {
           borderColor="gray.300"
           icon={<ArrowBackIcon />}
         />
+        {content}
       </Link>
-      <Card
-        direction={{ base: "column", sm: "row" }}
-        overflow="hidden"
-        variant="outline"
-      >
-        <Image
-          objectFit="cover"
-          maxW={{ base: "100%", sm: "200px" }}
-          src={`${MOVIEDB_IMAGES_URL}/${movie.poster_path}`}
-          alt={`${movie.title}'s poster`}
-        />
-
-        <Stack>
-          <CardBody>
-            <Heading size="md">{movie.title}</Heading>
-            <Text py="2" color="gray.600">
-              Rating: <Badge colorScheme="yellow">{movie?.vote_average}</Badge>
-            </Text>
-            <Text py="2">{movie.overview}</Text>
-          </CardBody>
-
-          <CardFooter>
-            <JokesGenerator
-              movieId={movie.id}
-              movieTitle={movie.title}
-              movieDescription={movie.overview}
-            />
-          </CardFooter>
-        </Stack>
-      </Card>
     </Box>
   );
 }
